6e17319ffac37ad8cd11c7f8fe7e1875ac7df1af
apollo994 <fabio.zanarello.94@gmail.com>
2025-04-13 18:00:05 +0200

    added description to key bindings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,14 +4,25 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- Default on_attach function for keymaps
 local on_attach = function(_, bufnr)
   local keymap = vim.keymap.set
-  local opts = { buffer = bufnr, silent = true }
 
-  keymap("n", "gd", vim.lsp.buf.definition, opts)
-  keymap("n", "K", vim.lsp.buf.hover, opts)
-  keymap("n", "gr", vim.lsp.buf.references, opts)
-  keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  keymap("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, silent = true, desc = "Go to definition" })
+  keymap("n", "K", vim.lsp.buf.hover, { buffer = bufnr, silent = true, desc = "Show hover info" })
+  keymap("n", "gr", vim.lsp.buf.references, { buffer = bufnr, silent = true, desc = "List references" })
+  keymap("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, silent = true, desc = "Rename symbol" })
+  keymap("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, silent = true, desc = "Code action" })
+  keymap("n", "<space>F", function()
+    vim.lsp.buf.format { async = true }
+    end, { buffer = bufnr, silent = true, desc = "Format buffer" })
 end
+
+-- How to render diagnostics
+vim.diagnostic.config({
+  virtual_text = true,     -- Show inline messages
+  signs = true,            -- Show icons in the gutter
+  underline = true,        -- Underline the problem
+  update_in_insert = true,
+  severity_sort = true,
+})
 
 -- Setup handlers
 local servers = { "pyright",
