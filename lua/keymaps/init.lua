8bf3e4a4989999b357bf6b6fdbd1a813ee5f5b40
apollo994 <fabio.zanarello.94@gmail.com>
2025-04-13 20:07:16 +0200

    add tabs

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,11 +1,15 @@
 local map = vim.keymap.set
 
 -- Buffer 
-map("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Close current buffer" })
+map("n", "<leader>bc", "<cmd>bd<CR>", { desc = "Close current buffer" })
+
+-- Tab
+map("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 
 -- Split windows
 map("n", "<leader>sv", "<cmd>vsplit<CR>", { desc = "Vertical split current buffer" })
 map("n", "<leader>sh", "<cmd>split<CR>", { desc = "Horizontal split current buffer" })
+map("n", "<leader>wc", "<C-w>c", { desc = "Close current window" })
 
 -- Window navigation using Ctrl + arrow keys
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left split" })
