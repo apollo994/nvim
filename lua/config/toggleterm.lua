1429452608b970c1d50696c575ba5d207f046ca9
apollo994 <fabio.zanarello.94@gmail.com>
2025-04-14 10:30:15 +0200

    floating terminal

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,7 +1,7 @@
 require("toggleterm").setup({
   size = 15,
   open_mapping = [[<C-\>]], -- Ctrl + \
-  direction = "horizontal", -- could be "float", "vertical"
+  direction = "float", -- could be "float", "vertical"
   shade_terminals = true,
   start_in_insert = true,
   persist_size = true,
